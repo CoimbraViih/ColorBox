23ea81a8f291a4c1bf0efc898dfedc2a459d5ee3
Victor Coimbra <viihcoimbra7x@gmail.com>
2026-06-17 11:12:53 -0300

    feat: add FloatingCTA fixed bottom bar for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import UrgencySection from './components/sections/UrgencySection'
 import GuaranteeSection from './components/sections/GuaranteeSection'
 import FAQSection from './components/sections/FAQSection'
 import FooterSection from './components/sections/FooterSection'
+import FloatingCTA from './components/ui/FloatingCTA'
 
 export default function App() {
   return (
-    <main className="min-h-screen">
-      <HeroSection />
-      <CharactersSection />
-      <BenefitsSection />
-      <TestimonialsSection />
-      <UrgencySection />
-      <GuaranteeSection />
-      <FAQSection />
-      <FooterSection />
-    </main>
+    <>
+      <main className="min-h-screen pb-20 md:pb-0">
+        <HeroSection />
+        <CharactersSection />
+        <BenefitsSection />
+        <TestimonialsSection />
+        <UrgencySection />
+        <GuaranteeSection />
+        <FAQSection />
+        <FooterSection />
+      </main>
+      <FloatingCTA />
+    </>
   )
 }
