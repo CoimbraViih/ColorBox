7ad73559042ace376536b87f9b2d7ddd0db965e2
Victor Coimbra <viihcoimbra7x@gmail.com>
2026-06-17 12:01:56 -0300

    feat: tema Noite Pixar CSS vars + App bootstrap GSAP

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import HeroSection from './components/sections/HeroSection'
-import CharactersSection from './components/sections/CharactersSection'
+import PainSection from './components/sections/PainSection'
 import BenefitsSection from './components/sections/BenefitsSection'
+import CharactersSection from './components/sections/CharactersSection'
 import TestimonialsSection from './components/sections/TestimonialsSection'
 import UrgencySection from './components/sections/UrgencySection'
 import GuaranteeSection from './components/sections/GuaranteeSection'
@@ -8,13 +12,19 @@ import FAQSection from './components/sections/FAQSection'
 import FooterSection from './components/sections/FooterSection'
 import FloatingCTA from './components/ui/FloatingCTA'
 
+gsap.registerPlugin(ScrollTrigger)
+
 export default function App() {
   return (
     <>
-      <main className="min-h-screen pb-20 md:pb-0">
+      <main
+        className="min-h-screen pb-20 md:pb-0"
+        style={{ background: 'linear-gradient(180deg, #1e1b4b 0%, #2d1b69 50%, #1e1b4b 100%)' }}
+      >
         <HeroSection />
-        <CharactersSection />
+        <PainSection />
         <BenefitsSection />
+        <CharactersSection />
         <TestimonialsSection />
         <UrgencySection />
         <GuaranteeSection />
---
+++ src/components/sections/PainSection.jsx
@@ -0,0 +1,1 @@
+export default function PainSection() { return null }
