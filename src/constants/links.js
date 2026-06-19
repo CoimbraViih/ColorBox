8b863c479abb5f96aa96ea2f2832f6a88080422c
Victor Coimbra <viihcoimbra7x@gmail.com>
2026-06-19 14:10:29 +0000

    Add ad creatives for Meta Ads campaign

--- src/constants/links.js
+++
@@ -1,5 +0,0 @@
-export const HOTMART_CHECKOUT_URL = 'https://pay.hotmart.com/I106262108Q'
-export const HOTMART_HOTLINK_URL  = 'https://go.hotmart.com/I106262108Q'
-export const HOTMART_PRODUCT_URL  = 'https://go.hotmart.com/I106262108Q?dp=1'
-
-export const CTA_TEXT = 'QUERO AGORA POR R$37'
